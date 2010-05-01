ee34f5c815b7caafac202953ec48d5161be87a5d
alberto <alberto.rodriguez@biko2.com>
2010-05-01 02:20:15 +0200

    Fixed indentation

--- src/notifier.py
+++ src/notifier.py
@@ -4,65 +4,66 @@ import pynotify
 import time
 
 class Notifier():
-  BASE_TITLE = 'Hudson Update!'
-  TIMEOUT = 1000
-  SUCCESS_IMG = 'file:///usr/share/pixmaps/gnome-suse.png'
-  UNSTABLE_IMG = 'file:///usr/share/pixmaps/gnome-suse.png'
-  FAILURE_IMG = 'file:///usr/share/pixmaps/gnome-suse.png'
-  url = 'http://www.newdawnsoftware.com/hudson/view/LWJGL/rssLatest'
+	BASE_TITLE = 'Hudson Update!'
+	TIMEOUT = 1000
+	SUCCESS_IMG = 'file:///usr/share/pixmaps/gnome-suse.png'
+	UNSTABLE_IMG = 'file:///usr/share/pixmaps/gnome-suse.png'
+	FAILURE_IMG = 'file:///usr/share/pixmaps/gnome-suse.png'
+	url = 'http://www.newdawnsoftware.com/hudson/view/LWJGL/rssLatest'
 
-  def __init__(self):
-	self.last_displayed = dict()
-	pynotify.init('Hudson Notify')
+	def __init__(self):
+		self.last_displayed = dict()
+		pynotify.init('Hudson Notify')
 
-  def success(self, job, build):
-	n = pynotify.Notification(self.BASE_TITLE,
+	def success(self, job, build):
+		n = pynotify.Notification(self.BASE_TITLE,
 	'"%s"  %s successfully built :)' % (job, build),
-	self.SUCCESS_IMG)
-	n.set_urgency(pynotify.URGENCY_LOW)
-	n.set_timeout(self.TIMEOUT)
-	return n
+		self.SUCCESS_IMG)
+		n.set_urgency(pynotify.URGENCY_LOW)
+		n.set_timeout(self.TIMEOUT)
+		return n
 
-  def unstable(self, job, build):
-	n = pynotify.Notification(self.BASE_TITLE,
+	def unstable(self, job, build):
+		n = pynotify.Notification(self.BASE_TITLE,
 		'"%s" %s is unstable :-/' % (job, build),
-	self.UNSTABLE_IMG)
-	n.set_timeout(self.TIMEOUT)
-	return n
+		self.UNSTABLE_IMG)
+		n.set_timeout(self.TIMEOUT)
+		return n
 
-  def failure(self, job, build):
-	n = pynotify.Notification(self.BASE_TITLE,
-	'"%s" %s failed!' % (job, build),
-	self.FAILURE_IMG)
-	n.set_urgency(pynotify.URGENCY_CRITICAL)
-	n.set_timeout(self.TIMEOUT)
-	return n
+	def failure(self, job, build):
+		n = pynotify.Notification(self.BASE_TITLE,
+		'"%s" %s failed!' % (job, build),
+		self.FAILURE_IMG)
+		n.set_urgency(pynotify.URGENCY_CRITICAL)
+		n.set_timeout(self.TIMEOUT)
+		return n
 
-  def main(self):
-	while True:
-		self.poll()
-		time.sleep(60)
+	def main(self):
+		while True:
+			self.poll()
+			time.sleep(60)
 
 
-  def poll(self):
-		print "UPDATING..."	
-		url = self.url
-		feed = feedparser.parse(url)
-		items = [t['title'] for t in feed['entries']]
-		for i in items:
-			i = i.split(' ')
-			job, build, status = (i[0], i[1], i[2])
-			
-			if job in self.last_displayed and self.last_displayed[job] == (build, status):
-				continue
-			self.last_displayed[job] = (build, status)
-			status = status.replace('(', '').replace(')','')
-			if status == 'SUCCESS':
-				self.success(job, build).show()
-			elif status == 'UNSTABLE':
-				self.unstable(job, build).show()
-			elif status == 'FAILURE':
-				self.failure(job, build).show()
-		return True
+def poll(self):
+	print "UPDATING..."
+	url = self.url
+	feed = feedparser.parse(url)
+	items = [t['title'] for t in feed['entries']]
+	for i in items:
+		i = i.split(' ')
+		job, build, status = (i[0], i[1], i[2])
+
+		if job in self.last_displayed and self.last_displayed[job] == (build, status):
+			continue
+		self.last_displayed[job] = (build, status)
+		status = status.replace('(', '').replace(')', '')
+		if status == 'SUCCESS':
+			self.success(job, build).show()
+		elif status == 'UNSTABLE':
+			self.unstable(job, build).show()
+		elif status == 'FAILURE':
+			self.failure(job, build).show()
+	return True
+
 if __name__ == '__main__':
 	Notifier().main()
--- src/tray.py
+++ src/tray.py
@@ -7,48 +7,49 @@ import gobject
 
 class HudsonTrayNotifier:
 
-  def __init__(self):
-    self.statusIcon = gtk.StatusIcon()
-    self.statusIcon.set_from_stock(gtk.STOCK_ABOUT)
-    self.statusIcon.set_visible(True)
-    self.statusIcon.set_tooltip("Hudson notifier")
+	def __init__(self):
+		self.statusIcon = gtk.StatusIcon()
+		self.statusIcon.set_from_stock(gtk.STOCK_ABOUT)
+		self.statusIcon.set_visible(True)
+		self.statusIcon.set_tooltip("Hudson notifier")
 
-    self.menu = gtk.Menu()
+		self.menu = gtk.Menu()
 #    self.menuItem = gtk.ImageMenuItem(gtk.STOCK_PREFERENCES)
 #    self.menuItem.connect('activate', self.prefs_cb, self.statusIcon)
 #    self.menu.append(self.menuItem)
-    self.menuItem = gtk.ImageMenuItem(gtk.STOCK_QUIT)
-    self.menuItem.connect('activate', self.quit_cb, self.statusIcon)
-    self.menu.append(self.menuItem)
+		self.menuItem = gtk.ImageMenuItem(gtk.STOCK_QUIT)
+		self.menuItem.connect('activate', self.quit_cb, self.statusIcon)
+		self.menu.append(self.menuItem)
 
-    self.statusIcon.connect('popup-menu', self.popup_menu_cb, self.menu)
-    self.statusIcon.set_visible(1)
-    self.notifier = Notifier()
-    gobject.timeout_add(5000, self.poll)
-    gtk.main()
+		self.statusIcon.connect('popup-menu', self.popup_menu_cb, self.menu)
+		self.statusIcon.set_visible(1)
+		self.notifier = Notifier()
+		gobject.timeout_add(5000, self.poll)
+		gtk.main()
 
-  def poll(self):
-    return self.notifier.poll()
+	def poll(self):
+		return self.notifier.poll()
 
-  def prefs_cb(self, widget, event, data = None):
-    window = gtk.Window(gtk.WINDOW_TOPLEVEL)
-    window.set_border_width(10)
+	def prefs_cb(self, widget, event, data = None):
+		window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+		window.set_border_width(10)
 
-    button = gtk.Button("Close")
-    button.connect_object("clicked", gtk.Widget.destroy, window)
-    window.add(button)
-    button.show()
-    window.show()
+		button = gtk.Button("Close")
+		button.connect_object("clicked", gtk.Widget.destroy, window)
+		window.add(button)
+		button.show()
+		window.show()
 
-  def quit_cb(self, widget, data = None):
-    gtk.main_quit()
+	def quit_cb(self, widget, data = None):
+		gtk.main_quit()
 
-  def popup_menu_cb(self, widget, button, time, data = None):
-    if button == 3:
-      if data:
-        data.show_all()
-        data.popup(None, None, gtk.status_icon_position_menu,
-                   3, time, self.statusIcon)
+	def popup_menu_cb(self, widget, button, time, data = None):
+		if button == 3:
+			if data:
+				data.show_all()
+				data.popup(None, None, gtk.status_icon_position_menu,
+						3, time, self.statusIcon)
 
 if __name__ == "__main__":
-  helloWord = HudsonTrayNotifier()
+	helloWord = HudsonTrayNotifier()
+
