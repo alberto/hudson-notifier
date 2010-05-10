1206e441d9e336b3540f30189ddd8c84cebead96
alberto <alberto.rodriguez@biko2.com>
2010-05-11 01:03:28 +0200

    Refactored hudson_poller. Added get_results method

--- src/hudson_poller.py
+++ src/hudson_poller.py
@@ -4,33 +4,38 @@ from rss_parser import RssParser
 
 class HudsonPoller():
 	def __init__(self, notifier):
-		self.last_displayed = dict()
+		self.job_results = dict()
 		self.notifier = notifier
 
 	def poll(self, urls):
 		for url in urls:
-			feed = feedparser.parse(url)
-			items = [t['title'] for t in feed['entries']]
-			for item in items:
-				job_result = self._get_build_info(item)
-				if self._is_old_build(job_result):
-					continue
+			job_results = self.__get_job_results(url)
+			for job_result in job_results:
+				if not self._is_old_build(job_result):
+					self.notifier.notify(job_result).show()
 				self.__add_job_result(job_result)
-
-				self.notifier.notify(job_result).show()
 		return True
+
+	def __get_job_results(self, url):
+		feed = feedparser.parse(url)
+		items = [t['title'] for t in feed['entries']]
+		return [self._get_build_info(item) for item in items]
 
 	def _get_build_info(self, item):
 		parser = RssParser()
 		return parser.parse(item)
 
 	def _is_old_build(self, job_result):
-		if not job_result.job in self.last_displayed:
+		if not job_result.job in self.job_results:
 			return False
-		last_build = self.last_displayed[job_result.job][0]
+
+		last_build = self.job_results[job_result.job].build_number
 		if job_result.build_number > last_build:
 			return False
 		return True
 
+	def get_results(self):
+		return self.job_results
+
 	def __add_job_result(self, job_result):
-		self.last_displayed[job_result.job] = (job_result.build_number, job_result.status)
+		self.job_results[job_result.job] = job_result
